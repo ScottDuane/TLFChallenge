aa0eeb591b1583c4dc822efb427ff950449374f4
Scott Duane <adrian.scott.duane@gmail.com>
2017-05-04 17:27:33 -0700

    Controller logic for event creation

--- app/controllers/recurring_events_controller.rb
+++ app/controllers/recurring_events_controller.rb
@@ -3,6 +3,16 @@ class RecurringEventsController < ApplicationController
     @recurring_events = RecurringEvent.all
   end
 
+  def create
+    @recurring_event = RecurringEvent.new(event_params)
+
+    if @recurring_event.save
+      redirect_to :root
+    else
+      flash.now[:errors].full_messages
+    end
+  end
+
   def new
   end
 
@@ -11,5 +21,9 @@ class RecurringEventsController < ApplicationController
 
   def destroy
   end
-  
+
+  private
+  def event_params
+    params.require(:recurring_event).permit(:name, :start_date, :interval, :buffer, :day_of_month)
+  end
 end
